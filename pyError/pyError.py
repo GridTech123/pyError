aa669ecad1ca7add783aadeaa278ef4a7d6e5fd0
GridTech <cdroach@live.com>
2016-09-19 18:29:44 -0500

    Add project files.

---
+++ pyError/pyError.py
@@ -0,0 +1,17 @@
+import tkMessageBox
+from Tkinter import *
+
+def newError(Title, HeadText, SubText, x, y):
+     root = Tk()
+     root.title(Title)
+     root["padx"] = x
+     root["pady"] = y
+
+     tkinterLabel = Label(root)
+     tkinterLabel["text"] = HeadText
+     tkinterLabel.pack()
+     tkinterLabel2 = Label(root)
+     tkinterLabel2["text"] = SubText
+     tkinterLabel2.pack()
+
+     root.mainloop()
